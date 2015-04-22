5cf0946e74e367a6a6630c367270a444d0298be6
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-04-22 09:48:23 -0400

    Added models and basic functions

---
+++ Assets/Scripts/Camera/camera_control.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_control : MonoBehaviour {
+	bool follow_player = true;
+	private Transform t;
+	private Transform pt;
+	private Vector3 move;
+	
+	void Start () {
+		if (follow_player){
+			pt = GameObject.Find ("player").transform;
+		}
+		t = gameObject.transform;
+	}
+
+	void Update () {
+		if (follow_player){
+			t.position = new Vector3(pt.position.x, pt.position.y, t.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/Controller/rotate_controller.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+var speed: float = 90.0;
+private var curEuler: Vector3;
+private var rotating: String = "";
+private var currentAngle: float;
+
+function Start () {
+	curEuler = transform.eulerAngles;
+}
+
+function RotateRight() {
+	if (rotating != "") return;
+	currentAngle -= 90;
+	RotateAngle(currentAngle);
+}
+
+function RotateLeft() {
+	if (rotating != "") return;
+	currentAngle += 90;
+	RotateAngle(currentAngle);
+}
+
+function RotateAngle(angle: float){
+	if (rotating != "") return;
+	if (angle > curEuler.z) rotating = "right";
+	else rotating = "left";
+	if (rotating == "right"){
+		while (curEuler.z < angle){
+			curEuler.z = Mathf.MoveTowards(curEuler.z, angle, speed*Time.deltaTime*-1);
+			transform.eulerAngles = curEuler;
+			yield;
+		}
+	}
+	else if (rotating == "left"){
+		while (curEuler.z > angle){
+			curEuler.z = Mathf.MoveTowards(curEuler.z, angle, speed*Time.deltaTime*-1);
+			transform.eulerAngles = curEuler;
+			yield;
+		}
+	}
+	rotating = "";
+}
